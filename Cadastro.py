4b7d6806c88f70157da0c2c469ab378aa556025e
bryanmichelf <michelbryan631@gmail.com>
2025-06-07 16:22:03 -0300

    primeiro commit do projeto A3

---
+++ Cadastro.py
@@ -0,0 +1,162 @@
+import sqlite3
+
+def tabela_jornada_academica():
+    conn = sqlite3.connect('jornada_academica.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS expositor (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            uc TEXT NOT NULL,
+            periodo TEXT,
+            curso TEXT,
+            titulo TEXT NOT NULL,
+            tipo_trabalho TEXT CHECK (tipo_trabalho IN ('extensão', 'pesquisa')),
+            localizacao TEXT NOT NULL
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS alunos_envolvidos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ra TEXT UNIQUE NOT NULL,
+            nome TEXT NOT NULL,
+            expositor_id INTEGER,
+            FOREIGN KEY (expositor_id) REFERENCES expositor(id) ON DELETE CASCADE
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS visitantes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+def inserir_expositor(uc, periodo, curso, titulo, tipo_trabalho, localizacao): 
+    conn = sqlite3.connect('jornada_academica.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO expositor 
+        (uc, periodo, curso, titulo, tipo_trabalho, localizacao) 
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (uc, periodo, curso, titulo, tipo_trabalho, localizacao))
+
+    conn.commit()
+    conn.close()
+
+def inserir_alunos_envolvidos(ra, nome, expositor_id):
+    conn = sqlite3.connect('jornada_academica.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO alunos_envolvidos
+        (ra, nome, expositor_id) 
+        VALUES (?, ?, ?)
+    ''', (ra, nome, expositor_id))
+
+    conn.commit()
+    conn.close()
+
+def inserir_visitantes(nome, email):
+    conn = sqlite3.connect('jornada_academica.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO visitantes
+        (nome, email)
+        VALUES (?, ?)
+    ''', (nome, email))
+
+    conn.commit()
+    conn.close()  
+
+def atualizar_expositor(id, uc, periodo, curso, titulo, tipo_trabalho, localizacao):
+    conn = sqlite3.connect('jornada_academica.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''  
+        UPDATE expositor 
+        SET uc = ?, periodo = ?, curso = ?, titulo = ?, tipo_trabalho = ?, localizacao = ?
+        WHERE id = ?
+    ''', (uc, periodo, curso, titulo, tipo_trabalho, localizacao, id))
+
+    conn.commit()
+    conn.close()
+
+def atualizar_alunos_envolvidos(id, ra, nome, expositor_id):
+    conn = sqlite3.connect('jornada_academica.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        UPDATE alunos_envolvidos 
+        SET ra = ?, nome = ?, expositor_id = ?
+        WHERE id = ?
+    ''', (ra, nome, expositor_id, id))
+
+    conn.commit()
+    conn.close()
+
+def atualizar_visitantes(id, nome, email):
+    conn = sqlite3.connect('jornada_academica.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        UPDATE visitantes           
+        SET nome = ?, email = ?
+        WHERE id = ?
+    ''', (nome, email, id))
+
+    conn.commit()
+    conn.close()
+
+def deletar_expositor(id):
+    conn = sqlite3.connect('jornada_academica.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        DELETE FROM expositor 
+        
+        WHERE id = ?', (id,)
+                   
+        ''')
+    
+    conn.commit()
+    conn.close()
+
+def deletar_alunos_envolvido(id):
+    conn = sqlite3.connect('jornada_academica.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        DELETE FROM alunos_envolvidos 
+    
+        WHERE id = ?', (id,)
+                   
+        ''')
+    
+    conn.commit()
+    conn.close()
+
+def deletar_visitante(id):
+    conn = sqlite3.connect('jornada_academica.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        DELETE FROM visitantes 
+                   
+        WHERE id = ?', (id,)
+                   
+     ''')
+    
+    conn.commit()
+    conn.close()
+
+    
+
+        
